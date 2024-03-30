f863debf96b6cc33c692370e5c7e9338dfb24427
Michelle <nyamburamichelle400@gmail.com>
2024-03-30 21:44:14 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+main.remove();
+
+const newHeader=document.createElement
+("h1")
+document.body.appendChild(newHeader);
+
+const element=document.querySelector('h1');
+element.id='victory';
+
+const adding=document.getElementById('victory');
+adding.innerHTML= "<p>Michelle is the champion</p>";
+
+
+
+
